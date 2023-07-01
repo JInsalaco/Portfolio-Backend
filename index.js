bd550d8135793f963e28537b2775a662f497f3a5
Joseph Insalaco <joseph.insalaco1@gmail.com>
2023-07-01 14:58:38 -0400

    Changing port for heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const configRoutes = require("./routes");
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 const corsOptions = {
   // all network requests allowed from the frontend URL only
