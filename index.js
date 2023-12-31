311c339b5e53bd62509ba7f6dccb3b82218a3c87
Joseph insalaco <joseph.insalaco1@gmail.com>
2023-12-30 21:45:22 -0500

    Added a parentheses by mistake

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(express.json({ limit: "50mb" })));
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true}));
 
 configRoutes(app);
